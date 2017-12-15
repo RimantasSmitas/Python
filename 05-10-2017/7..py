0b7e9ef175d14467d3e7b23e8257b0ecc34cb9e3
rimantassmitas <Rimantas.smitas@gmail.com>
2017-12-15 17:00:30 +0100

    Reorder

--- 05-10-2017/7..py
+++
@@ -1,4 +0,0 @@
-value1 = 2.0
-value2 = 12
-result = value1 * value2
-print(result)
---
+++ CH5.Functions/7.StadiumSeating.py
@@ -0,0 +1,18 @@
+aSeat = 20
+bSeat = 15
+cSeat = 10
+
+
+def main():
+    aCount = int(input("How many a seats were sold?"))
+    bCount = int(input("How many b seats were sold?"))
+    cCount = int(input("How many c seats were sold?"))
+    Calculator(aCount,bCount,cCount)
+
+def Calculator(aCount,bCount,cCount):
+    totalprice = (aCount * aSeat) + (bCount*bSeat)+(cCount+cSeat)
+
+
+
+
+main()
